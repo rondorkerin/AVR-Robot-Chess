93278521dcfbfa034d5b228316bb5e3872625a5e
Nick Bryant <sbryant31@gmail.com>
2012-08-16 20:29:38 -0400

    Created guide for programming the Xplained as well as a chess test

--- src/main.c
+++ src/main.c
@@ -14,6 +14,19 @@
 #include <stdio.h>
 #include "chessengine.h"
 
+#ifdef __GNUC__
+# include <util/delay.h>
+#else
+static inline void _delay_ms(uint16_t t)
+{
+	uint16_t i;
+
+	for (i = 0; i < t; ++i) {
+		__delay_cycles((UINT32_C)(sysclk_get_cpu_hz() / 6000));
+	}
+}
+#endif
+
 static inline void write_board_led_mask(uint8_t mask)
 {
 	LED_IOPORT.OUT = ~mask;
@@ -28,27 +41,56 @@ static inline void write_board_led_mask(uint8_t mask)
 int main (void)
 {
 	board_init();
-6
+
 	// Insert application code here, after the board has been initialized.	
 	gpio_set_pin_low(LED0_GPIO);
 	
 	while (1){		
+		
 		InitEngine();
 		InitGame(PlayerVsAI);
 
 		int gameResult = 0, tookPieceFlag = 0,illegalMoveFlag = 0;
 		char resultMove[4];
 
-		for (;;) {
-			char cmd[100] = "a2a4";
-			PlayerMove(cmd, &gameResult, &tookPieceFlag, &illegalMoveFlag);
+		write_board_led_mask(0xDD);
+		_delay_ms(4000);
+		write_board_led_mask(0x00);
+		
+		char cmd[3][100] = {"a2a4", "a4a5", "a5a6"};
+		int i;
+		for (i = 0; i < 5; i++)
+		{
+			PlayerMove(cmd[i], &gameResult, &tookPieceFlag, &illegalMoveFlag);
 			if (illegalMoveFlag)
 			{
 				//printf("illegal move.\n");
 				continue;
 			}
 			AIMove(&gameResult, &tookPieceFlag, resultMove);
+					
+			if (!tookPieceFlag)
+			{
+				write_board_led_mask(0x0F);
+				_delay_ms(2000);
+				write_board_led_mask(0x00);
+				_delay_ms(2000);
+			}
+			else
+			{
+				for (;;) {
+					write_board_led_mask(0xF0);
+					_delay_ms(200);
+					write_board_led_mask(0x00);
+					_delay_ms(200);
+							
+				}
+						
+			}
 		}
+
+
+		
 		
 	}
 	
