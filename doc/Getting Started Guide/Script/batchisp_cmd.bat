4312b8755650e9a04b7c78f3bea607288ee739b8
Nick Bryant <sbryant31@gmail.com>
2012-08-21 20:09:11 -0400

    Added arduino sketch for serial debugging

--- doc/Getting Started Guide/Script/batchisp_cmd.bat
+++
@@ -1,2 +0,0 @@
-batchisp -cmdfile commands.txt
-pause
--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include <asf.h>
 #include <gpio.h>
 #include <stdio.h>
+#include <avr/io.h>
+#include <stdbool.h>
 #include "chessengine.h"
 
 #ifdef __GNUC__
@@ -27,10 +29,34 @@ static inline void _delay_ms(uint16_t t)
 }
 #endif
 
+
 static inline void write_board_led_mask(uint8_t mask)
 {
 	LED_IOPORT.OUT = ~mask;
 }
+
+/************************************************************************/
+/* Sends a debug message to the arduino serial monitor                  */
+/* Hook pin D3 of the Xmega up to Pin 2 of the arduino	                */
+/************************************************************************/
+void debug_msg(char *message)
+{	
+	int i = 0;
+    while(message[i] != '\0') //hacked because sizeof doesnt work???
+    {
+        // Transmit a character, first wait for the previous to be sent
+        while( (USARTD0.STATUS & USART_DREIF_bm) == 0 ) {}
+       	  // Now, transmit the character
+        USARTD0.DATA = message[i];      
+
+        i++;
+    }
+	USARTD0.DATA = '_';    
+	while( (USARTD0.STATUS & USART_DREIF_bm) == 0 ) {}
+	// carriage return and line feed dont work
+}
+
+
 /*
  * TEST MAIN: 
  * Issues a sequence of pawn movements and blinks an LED if a piece was taken. 
@@ -42,9 +68,14 @@ int main (void)
 {
 	board_init();
 
-	// Insert application code here, after the board has been initialized.	
-	gpio_set_pin_low(LED0_GPIO);
+	// SET UP SERIAL DEBUGGING VIA ARDUINO (D3 is TX for Xmega) D3 -> D2, D2-> D3, Gnd->Gnd)
+	PORTD.DIRSET   = PIN3_bm;   // Pin 3 (TXD0) as output.
+	USARTD0.CTRLC = (uint8_t) USART_CHSIZE_8BIT_gc | USART_PMODE_DISABLED_gc | false; // USARTD0, 8 Data bits, No Parity, 1 Stop bit
+	USARTD0.BAUDCTRLA = 12;
+	USARTD0.CTRLB |= USART_TXEN_bm | USART_RXEN_bm; // Enable both RX and TX
 	
+	debug_msg("init game engine.");
+			
 	while (1){		
 		
 		InitEngine();
@@ -53,39 +84,32 @@ int main (void)
 		int gameResult = 0, tookPieceFlag = 0,illegalMoveFlag = 0;
 		char resultMove[4];
 
-		write_board_led_mask(0xDD);
-		_delay_ms(4000);
-		write_board_led_mask(0x00);
 		
 		char cmd[3][100] = {"a2a4", "a4a5", "a5a6"};
 		int i;
 		for (i = 0; i < 5; i++)
 		{
 			PlayerMove(cmd[i], &gameResult, &tookPieceFlag, &illegalMoveFlag);
+			debug_msg("player mv:");
+			debug_msg(cmd[i]);
+			
 			if (illegalMoveFlag)
 			{
-				//printf("illegal move.\n");
+				debug_msg("illegal move");
 				continue;
 			}
 			AIMove(&gameResult, &tookPieceFlag, resultMove);
 					
 			if (!tookPieceFlag)
 			{
-				write_board_led_mask(0x0F);
-				_delay_ms(2000);
-				write_board_led_mask(0x00);
-				_delay_ms(2000);
+				debug_msg("ai mv:");
+				debug_msg(resultMove);
 			}
 			else
 			{
-				for (;;) {
-					write_board_led_mask(0xF0);
-					_delay_ms(200);
-					write_board_led_mask(0x00);
-					_delay_ms(200);
-							
-				}
-						
+				debug_msg("AI took player piece");
+				for (;;) {						
+				}	
 			}
 		}
 
